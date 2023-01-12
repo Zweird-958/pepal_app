526646f6791735f45b5d283067062091d63f1537
Zweird-958 <xdfamilyytt@gmail.com>
2023-01-12 15:44:48 +0100

    deleted

--- PepalApp/app/src/main/java/classes/Renders.kt
+++
@@ -1,4 +0,0 @@
-package classes
-
-class Renders (id: Int,matiere: String,val description: String, heureDebut: String,dateDebut : String,heureFin: String,dateFin: String) : Assignment(id,matiere,heureDebut,dateDebut,heureFin,dateFin) {
-}
