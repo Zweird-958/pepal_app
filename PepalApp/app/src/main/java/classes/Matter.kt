ea0f6ac94f37ea58639734e92460dc48db1fde6a
Zweird-958 <xdfamilyytt@gmail.com>
2023-01-12 13:44:11 +0100

    delete numberOfMarks parameter

--- PepalApp/app/src/main/java/classes/Matter.kt
+++ PepalApp/app/src/main/java/classes/Matter.kt
@@ -2,13 +2,17 @@ package classes
 
 var mattersList = mutableListOf<Matter>()
 
-class Matter (val name: String, var numberOfMarks: Int, var totalMarks: Float){
+class Matter (val name: String, var totalMarks: Float){
 
-    constructor(name: String, numberOfMarks: Int, totalMarks: Float,mattersList: MutableList<Matter>) :
-            this(name, numberOfMarks, totalMarks) {
-        if (mattersList.any { it.name == name }) {
-            println("Error : Name already taken")
+    public var numberOfMarks: Int = 0
+
+    constructor(name: String, totalMarks: Float, mattersList: MutableList<Matter>) :
+            this(name, totalMarks) {
+        val matter = mattersList.firstOrNull { it.name == name }
+        if (matter != null) {
+            matter.addNewMark(totalMarks)
         } else {
+            this.numberOfMarks = 1
             mattersList.add(this)
         }
     }
