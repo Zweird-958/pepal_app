8f48a15e7794506420251153a65ef59086a01b2d
Zweird-958 <xdfamilyytt@gmail.com>
2023-01-12 15:44:29 +0100

    toString

--- PepalApp/app/src/main/java/classes/Assignment.kt
+++ PepalApp/app/src/main/java/classes/Assignment.kt
@@ -1,3 +1,15 @@
 package classes
 
-abstract class Assignment (val id: Int,val matiere: String,val heureDebut: String,val dateDebut : String, val heureFin: String, val dateFin: String)
+import kotlin.time.measureTime
+
+abstract class Assignment (val id: Int,val matiere: String,val heureDebut: String,val dateDebut : String, val heureFin: String, val dateFin: String){
+    override fun toString(): String {
+        println(id)
+        println(matiere)
+        println(heureDebut)
+        println(heureFin)
+        println(dateDebut)
+        println(dateFin)
+        return ""
+    }
+}
