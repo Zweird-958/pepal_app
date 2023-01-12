d727aaf4748a8f39f154ea520a799c746a7fa044
Zweird-958 <xdfamilyytt@gmail.com>
2023-01-12 16:00:32 +0100

    comment .any

--- PepalApp/app/src/main/java/classes/Course.kt
+++ PepalApp/app/src/main/java/classes/Course.kt
@@ -4,8 +4,8 @@ var courseList = mutableListOf<Course>()
 
 class Course (id: Int,matiere: String,val intervenant: String, val salle: String,heureDebut: String?,dateDebut : String?,heureFin: String?,dateFin: String?) : Assignment(id,matiere,heureDebut,dateDebut,heureFin,dateFin) {
     constructor(id: Int,matiere: String,intervenant: String,   salle: String,heureDebut: String?,dateDebut : String?,heureFin: String?,dateFin: String?, courseList: MutableList<Course>) : this(id,matiere,intervenant,salle,heureDebut,dateDebut,heureFin,dateFin){
-        if (courseList.any { it.id != id }){
+        //if (courseList.any { it.id != id }){
             courseList.add(this)
-        }
+        //}
     }
 }
