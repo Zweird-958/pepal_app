cd2222aabf40d2681746267bafed43b8a83403d5
Zweird-958 <xdfamilyytt@gmail.com>
2023-01-12 13:13:00 +0100

    creation of Matter Class

--- PepalApp/app/src/main/java/classes/Matter.kt
+++ PepalApp/app/src/main/java/classes/Matter.kt
@@ -1,4 +1,21 @@
 package classes
 
-class Matter {
+var mattersList = listOf<Matter>()
+
+class Matter (val name: String, var numberOfMarks: Int, var totalMarks: Float){
+
+    constructor(name: String, numberOfMarks: Int, totalMarks: Float, mattersList: MutableList<Matter>) : this(name, numberOfMarks, totalMarks) {
+        if (mattersList.any { it.name == name }) {
+            println("Error : Name already taken")
+        } else {
+            mattersList.add(this)
+        }
+    }
+
+
+    fun addNewMark(newMark: Float){
+        this.numberOfMarks += 1
+        this.totalMarks += newMark
+    }
+
 }
